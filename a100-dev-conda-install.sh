8c0ec56d1a76f4a3216a29257f35b1d5d148447c
unknown <7614175+jsnng@users.noreply.github.com>
2022-11-03 22:38:20 +1100

    fixed path

--- a100-dev-conda-install.sh
+++ a100-dev-conda-install.sh
@@ -5,9 +5,9 @@ module load anaconda/conda3
 ```
 
 cd "/bigdata/users/postgrad/jng1/"
-[ ! -d ".conda/dev/" ] && mkdir -p "./conda/dev/"
-conda create -y -p ".conda/dev/echocv" -c free python=2.7.11 anaconda
-conda activate "~/.conda/dev/echocv"
+[ ! -d ".conda/" ] && mkdir -p "./conda/"
+conda create -y -p ".conda/echocv" -c free python=2.7.11 anaconda
+conda activate "~/.conda/echocv"
 
 git clone https://bitbucket.com/rahuldeo/echocv/src/master
 pip install --no-cache-dir -r echocv/requirements.txt
