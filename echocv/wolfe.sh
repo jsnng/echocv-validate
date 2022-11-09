a8d6b77fde2fb78b99d8c54db725eb6eb9eacf28
jng1 <jng1@a100-dev.cdms.hpc>
2022-11-09 12:00:34 +1100

    wolfe setup script fixed

--- echocv/wolfe.sh
+++ echocv/wolfe.sh
@@ -1,14 +1,14 @@
-!# /usr/bin/env bash
+#! /usr/bin/env bash
 
-```
+module purge
 module load anaconda/conda3
-```
-
-cd "/bigdata/users/postgrad/jng1/"
-[ ! -d ".conda/" ] && mkdir -p "./conda/"
-conda create -y -p ".conda/echocv" -c free python=2.7.11 anaconda
-conda activate "~/.conda/echocv"
 
 git clone https://bitbucket.com/rahuldeo/echocv/src/master
-pip install --no-cache-dir -r echocv/requirements.txt
-pip install --no-cache-dir easydict==1.9
+
+if [ $? -eq 0 ]; then
+	conda create -y -p "echocv" -c free python=2.7.11 anaconda
+	conda activate "echocv"
+
+	pip install --no-cache-dir -r echocv/requirements.txt
+	pip install --no-cache-dir easydict==1.9
+fi
--- echonet/wolfe.sh
+++ echonet/wolfe.sh
@@ -1,10 +1,13 @@
 #! /usr/bin/env bash
 
+module purge
+module load anaconda/conda3
+
 git clone https://github.com/echonet/dynamic
 cd "$(realpath .)/dynamic"
 
-#cat requirements.txt | sed s'/echonet==1.0.0"//' | xargs -n 1 pip install --quiet
-#pip install click
+cat requirements.txt | sed s'/echonet==1.0.0"//' | xargs -n 1 pip install --quiet
+pip install click
 
 mkdir "$(realpath .)/dynamic/models"
 wget -P models/ https://github.com/douyang/EchoNetDynamic/releases/download/v1.0.0/deeplabv3_resnet50_random.pt 
