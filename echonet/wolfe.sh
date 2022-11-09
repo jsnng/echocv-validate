9d62d0bbc415c73a570aad11517628376cdaae04
jng1 <jng1@a100-dev.cdms.hpc>
2022-11-09 16:44:56 +1100

    fixed setup scripts

--- echonet/wolfe.sh
+++ echonet/wolfe.sh
@@ -3,11 +3,11 @@
 module purge
 module load anaconda/conda3
 
-git clone https://github.com/echonet/dynamic
-cd "$(realpath .)/dynamic"
+git clone https://github.com/echonet/dynamic 
+conda create -p "echonet" -c conda-forge python=3.8.1
 
-cat requirements.txt | sed s'/echonet==1.0.0"//' | xargs -n 1 pip install --quiet
-pip install click
+#cat dynamic/requirements.txt | sed s'/echonet==1.0.0"//' | xargs -n 1 pip install --quiet
+#pip install click
 
 mkdir "$(realpath .)/dynamic/models"
 wget -P models/ https://github.com/douyang/EchoNetDynamic/releases/download/v1.0.0/deeplabv3_resnet50_random.pt 
