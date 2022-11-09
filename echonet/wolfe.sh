e3ee8020f788b82f6921b259de27e61a4757d954
jng1 <jng1@a100-dev.cdms.hpc>
2022-11-09 17:21:21 +1100

    fixed typos

--- echonet/wolfe.sh
+++ echonet/wolfe.sh
@@ -4,10 +4,11 @@ module purge
 module load anaconda/conda3
 
 git clone https://github.com/echonet/dynamic 
-conda create -p "echonet" -c conda-forge python=3.8.1
+conda create -p ".echonet" -y -c conda-forge python=3.8.1 anaconda
+conda activate "$(realpath .)/.echonet/"
 
-#cat dynamic/requirements.txt | sed s'/echonet==1.0.0"//' | xargs -n 1 pip install --quiet
-#pip install click
+cat dynamic/requirements.txt | sed "s/echonet==1.0.0//" | xargs -n 1 pip install --no-cache-dir 
+pip install click
 
 mkdir "$(realpath .)/dynamic/models"
 wget -P models/ https://github.com/douyang/EchoNetDynamic/releases/download/v1.0.0/deeplabv3_resnet50_random.pt 
