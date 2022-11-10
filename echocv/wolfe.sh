5593215113df1ed4942d4dae8374a23e72890ebe
Jason Ng <7614175+jsnng@users.noreply.github.com>
2022-11-10 20:28:19 +1100

    clean up

--- echocv/wolfe.sh
+++ echocv/wolfe.sh
@@ -5,10 +5,10 @@ module load anaconda/conda3
 
 git clone https://bitbucket.com/rahuldeo/echocv/src/master
 
-conda create -y -p "echocv" -c free python=2.7.11 anaconda
-conda activate "$(realpath .)/echocv/"
+conda create -y -p ".echocv" -c free python=2.7.11 anaconda
+conda activate "$(realpath .)/.echocv/"
 
 pip install --no-cache-dir -r master/requirements.txt
 pip install --no-cache-dir easydict==1.9
 
-wget -O data.zip https://www.dropbox.com/sh/0tkcf7e0ljgs0b8/AACBnNiXZ7PetYeCcvb-Z9MSa?dl=0
+#wget -O data.zip https://www.dropbox.com/sh/0tkcf7e0ljgs0b8/AACBnNiXZ7PetYeCcvb-Z9MSa?dl=0
