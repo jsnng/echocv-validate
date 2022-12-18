ceac735f93074227c5891e7ebdc35829fe67de5d
jng1 <jng1@a100-dev.cdms.hpc>
2022-12-18 19:21:20 +1100

    wolfe setup script no longer uses virtual environment

--- echonet/wolfe.sh
+++ echonet/wolfe.sh
@@ -1,15 +1,16 @@
 #! /usr/bin/env bash
 
 module purge
-module load anaconda/conda3
+module load Python/Python3.9
 
 git clone https://github.com/echonet/dynamic 
-conda create -p ".echonet" -y -c conda-forge python=3.8.1 anaconda
-conda activate "$(realpath .)/.echonet/"
 
-cat dynamic/requirements.txt | sed "s/echonet==1.0.0//" | xargs -n 1 pip install --no-cache-dir 
-pip install --no-cache-dir click
+cd "$(realpath .)/dynamic"
+pip install .
 
 mkdir "$(realpath .)/dynamic/models"
 wget -P models/ https://github.com/douyang/EchoNetDynamic/releases/download/v1.0.0/deeplabv3_resnet50_random.pt 
 wget -P models/ https://github.com/douyang/EchoNetDynamic/releases/download/v1.0.0/r2plus1d_18_32_2_pretrained.pt
+
+if [ -f "EchoNet-Dynamic.zip" ] && unzip EchoNet-Dynamic.zip
+echo 'DATA_DIR = EchoNet-Dynamic/' > echonet.cfg
